d4ffd91b82fefaf2296581062756de8d068b9909
Nurdin Ismail <nurdin.ismail@student.moringaschool.com>
2023-09-05 09:42:52 +0300

    finished setup

---
+++ lib/migrations/versions/3011b4a511e0_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 3011b4a511e0
+Revises: 
+Create Date: 2023-09-05 09:38:06.501052
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3011b4a511e0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
